36e3a7fcee7361fbd72d4ac84f4a409158a02254
Hollis Brown-236 <123035058+Hollis-Brown@users.noreply.github.com>
2023-08-21 21:09:34 -0700

    Update script.js
    Updated JavaScript file.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-document.querySelector('#help').addEventListener('click', stopSnacking)
+document.querySelector('#help').addEventListener('click', stopSnacking);
 
 function stopSnacking() {
-	let numOfSnacks = document.querySelector('input').value;
-	document.querySelector('#stops').innerText = ""
-	for(let i = 1; i<= numOfSnacks; i++) {
-		document.querySelector('#stops').innerText += ' STOP!'
-	}
+  let numOfSnacks = parseInt(document.querySelector('#snacksInput').value);
+  document.querySelector('#stops').innerText = "";
+  for (let i = 1; i <= numOfSnacks; i++) {
+    document.querySelector('#stops').innerText += ' STOP!';
+  }
 }
